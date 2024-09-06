31c1e1a59d9c281b03fae207b6dbacf5000bf653
Jon <90940611+jonklee99@users.noreply.github.com>
2024-09-06 13:29:46 -0500

    Update Ban Command

--- SysBot.Pokemon.Discord/SysCord.cs
+++ SysBot.Pokemon.Discord/SysCord.cs
@@ -246,12 +246,12 @@ namespace SysBot.Pokemon.Discord
             var mgr = Manager;
             if (!mgr.CanUseCommandUser(msg.Author.Id))
             {
-                await msg.Channel.SendMessageAsync("You are not permitted to use this command.").ConfigureAwait(false);
+                await msg.Channel.SendMessageAsync($"{msg.Author.Mention}, You are blacklisted from using the bots! Contact the bot owner <@754156803906076712>!").ConfigureAwait(false);
                 return true;
             }
             if (!mgr.CanUseCommandChannel(msg.Channel.Id) && msg.Author.Id != mgr.Owner)
             {
-                await msg.Channel.SendMessageAsync("You can't use that command here.").ConfigureAwait(false);
+                await msg.Channel.SendMessageAsync($"{msg.Author.Mention}, You can't use that command here.").ConfigureAwait(false);
                 return true;
             }
 
